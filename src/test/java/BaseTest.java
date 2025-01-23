31b097e0b40028bdbf2a0fe920552064f8c55761
TaslimaTaherAely <aelytaher@gmail.com>
2025-01-23 22:34:08 +0600

    test register

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,6 +1,8 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class BaseTest {
@@ -10,6 +12,22 @@ public class BaseTest {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://tutorialsninja.com/demo/index.php");
+        driver.manage().window().maximize();
+        driver.findElement(By.xpath("//a[@title='My Account']")).click();
+        driver.findElement(By.xpath("//a[text()='Register']")).click();
+        driver.findElement(By.xpath("//input[@id='input-firstname']")).sendKeys("Taslima Taher");
+        driver.findElement(By.xpath("//input[@id='input-lastname']")).sendKeys("Aely");
+        driver.findElement(By.xpath("//input[@id='input-email']")).sendKeys("aelytaher23@gmail.com");
+        driver.findElement(By.xpath("//input[@id='input-telephone']")).sendKeys("+880187123456");
+        driver.findElement(By.xpath("//input[@id='input-password']")).sendKeys("aely1234");
+        driver.findElement(By.xpath("//input[@id='input-confirm']")).sendKeys("aely1234");
+        driver.findElement(By.xpath("//input[@name='newsletter' and @value='1']")).click();
+        driver.findElement(By.xpath("//input[@name='agree']")).click();
+        driver.findElement(By.xpath("//input[@type='submit']")).click();
+        String thankYouText = driver.findElement(By.xpath("//h1[text()='Your Account Has Been Created!']")).getText();
+        Assert.assertEquals(thankYouText, "Your Account Has Been Created!");
+
+
 
     }
 
