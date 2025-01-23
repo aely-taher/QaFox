6b3e35f23b7fae0e5aa4114e2a8b50a6199b7318
TaslimaTaherAely <aelytaher@gmail.com>
2025-01-24 00:02:06 +0600

    find element and find elements

---
+++ src/test/java/FindElement.java
@@ -0,0 +1,32 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class FindElement {
+
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+        WebElement searchBar = driver.findElement(By.name("q"));
+        searchBar.sendKeys("Streams Tech Limited" + Keys.ENTER);
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        List<WebElement> searchList = driver.findElements(By.xpath("//span/a/h3"));
+        for(WebElement searchItem : searchList) {
+            System.out.println(searchItem.getText());
+        }
+        driver.manage().window().maximize();
+
+
+
+    }
+}
